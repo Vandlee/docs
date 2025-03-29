0b98d76d26690beb3741b836f5554f415f0bad9f
Softy <79613492+elcheryu-u@users.noreply.github.com>
2025-03-29 04:38:11 -0500

    vercel deploy v1.0.1-1

--- docs/pages/u-docs/html/element-a.js
+++ docs/pages/u-docs/html/element-a.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocsV2';
 import AppFrame from 'docs/src/modules/components/AppFrame';
+import MarkdownDocsV2 from 'docs/src/modules/components/MarkdownDocsV2';
 import * as pageProps from 'docs/data/u-docs/html/elements/a/a.md?vandleeMarkdown';
 
 export default function Page() {
-  return <MarkdownDocs {...pageProps} />;
+  return <MarkdownDocsV2 {...pageProps} />;
 }
 
 Page.getLayout = (page) => {
